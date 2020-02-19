3cc5f6191195e4a3e30b06df898ef7aa0a428977
kristylym <36501827+kristylym@users.noreply.github.com>
2020-02-19 11:10:04 +0600

    add file

--- src/index.js
+++ src/index.js
@@ -1,13 +1,5 @@
 
 import './assets/scss/main.scss'
-// SmoothScroll
-var scroll = new SmoothScroll('li>a[href*="#"]');
-// Swiper
-let Swiper = require('./js/swiper.min.js')
-let swiper = new Swiper('.swiper-container', {
-    direction: 'vertical',
-    pagination: {
-      el: '.swiper-pagination',
-      clickable: true,
-    },
-  });
+$('.mobile_menu').click(function(){
+  $('.header-menu__category').fadeToggle();
+});
